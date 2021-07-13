e7ea579d0255d0abe847d0f878922abc89f37dcb
Gerald <gejocadev@gmail.com>
2021-07-13 16:04:03 -0500

    desarrollo de pagina curso

--- js/log.js
+++ js/log.js
@@ -1,6 +1,18 @@
+function functionSubmit(e){
+    e.preventDefault();
+ }
+
 function login(){
 
+    
+    
+    let correo = "francisco.javier@gmail.com", correoing = document.getElementById('correoing').value;
+    
+    if (correo == correoing){
+        window.location.href = "home.html";
+    }else{
+        alert ("ingrese un correo valido");
+    }
 
-    correo = document.getElementById('correo').value;
-    if ()
 }
+
